f11188cc7d856d5ba97b6480e3addcb3eff1d53e
nkisel <nkisel@berkeley.edu>
2019-01-03 10:05:02 -0800

    Navigation bar and some links
    Have yet to implement any text, scripts

---
+++ scripts/logo.js
@@ -0,0 +1,20 @@
+// When the logo is moused over, its opacity steadily fades to 80%. Otherwise, it steadily increases to 100%.
+
+if (the pointer is on the logo) {
+
+    if (opacity <= 80) {
+
+        break;
+
+    } else {
+
+        opacity = opacity - 2;
+
+    }
+
+} else {
+
+opacity = opacity + 2
+
+}
+
---
+++ scripts/top.js
@@ -0,0 +1,17 @@
+
+//if {}
+//else {}
+
+document.getElementById("top")
+
+function shrinkTop() {
+    if (document.body.scrollTop > 50) {
+
+        document.getElementById("logo").style.padding_left = "10px";
+
+    } else {
+
+
+    }
+
+}
