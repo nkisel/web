a915fea8a826ab7e31bd3acd49ca5bb1709e863b
Nick Kisel <nkisel@berkeley.edu>
2019-12-06 22:01:28 -0800

    Began work on new website. Homepage complete.

--- scripts/top.js
+++ scripts/top.js
@@ -8,6 +8,7 @@ function shrinkTop() {
     if (document.body.scrollTop > 50) {
 
         document.getElementById("logo").style.padding_left = "10px";
+        document.getElementById("logo").style.scale = "10px";
 
     } else {
 
