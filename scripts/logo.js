1534d23c56a1a84293e5863986c413170caf8892
Nick Kisel <nkisel@berkeley.edu>
2019-12-23 13:54:42 -0800

    JavaScript for darkening all unfocused elements

--- scripts/logo.js
+++ scripts/logo.js
@@ -1,20 +1,40 @@
 // When the logo is moused over, its opacity steadily fades to 80%. Otherwise, it steadily increases to 100%.
 
-if (the pointer is on the logo) {
+var content = document.getElementsByClassName("content")[0];
+var profile = document.getElementById("profile");
+var profile_name = document.getElementById("profile-name");
+var buttons = document.getElementById("links");
+var button = document.getElementsByClassName("button");
+//let logo = logodiv.getElementsByTagName("img")[0];
+buttons.style.opacity = 0;
 
-    if (opacity <= 80) {
-
-        break;
-
-    } else {
-
-        opacity = opacity - 2;
-
-    }
-
-} else {
-
-opacity = opacity + 2
-
+function makeTransparent(elem, level) {
+    elem.style.opacity = level;
 }
+
+function makeOpaque(elem) {
+    if (elem.style.opacity < 1) {
+        elem.style.opacity = 1;
+    }
+}
+
+content.onmouseenter = function() {
+    makeOpaque(buttons);
+}
+
+buttons.onmouseenter = function() {
+    for (i = 0; i < button.length; i++) {
+        button[i].classList.add("unfocused");
+    }
+    makeTransparent(profile, 0.8);
+    makeTransparent(profile_name, 0.6);
+}
+buttons.onmouseleave = function() {
+    for (i = 0; i < button.length; i++) {
+        button[i].classList.remove("unfocused");
+    }
+    makeOpaque(profile);
+    makeOpaque(profile_name);
+}
+
 
