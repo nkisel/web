71484c3ea0fd98a01dd19c2a3381a58dff40413c
Nick Kisel <nkisel@berkeley.edu>
2020-01-01 11:00:53 -0800

    Fix scaling and update documents

--- scripts/logo.js
+++ scripts/logo.js
@@ -2,11 +2,15 @@
 
 var content = document.getElementsByClassName("content")[0];
 var profile = document.getElementById("profile");
-var profile_name = document.getElementById("profile-name");
+var profilename = document.getElementById("profile-name");
 var buttons = document.getElementById("links");
 var button = document.getElementsByClassName("button");
+var widthcounter = document.getElementById("width");
+//|| document.documentElement.clientWidth
+//|| document.body.clientWidth;
 //let logo = logodiv.getElementsByTagName("img")[0];
-buttons.style.opacity = 0;
+buttons.style.opacity = 0.2;
+this.profilename.style.fontSize = window.innerWidth / 12 + "px";
 
 function makeTransparent(elem, level) {
     elem.style.opacity = level;
@@ -27,14 +31,34 @@ buttons.onmouseenter = function() {
         button[i].classList.add("unfocused");
     }
     makeTransparent(profile, 0.8);
-    makeTransparent(profile_name, 0.6);
+    makeTransparent(profilename, 0.6);
 }
 buttons.onmouseleave = function() {
     for (i = 0; i < button.length; i++) {
         button[i].classList.remove("unfocused");
     }
     makeOpaque(profile);
-    makeOpaque(profile_name);
+    makeOpaque(profilename);
 }
 
+/*window.onresize = function () {
+    this.profilename.style.fontSize = width / 15;
+    this.profilename.textContent = width;
+}*/
 
+var addEvent = function(object, type, callback) {
+    if (object == null || typeof(object) == 'undefined') return;
+    if (object.addEventListener) {
+        object.addEventListener(type, callback, false);
+    } else if (object.attachEvent) {
+        object.attachEvent("on" + type, callback);
+    } else {
+        object["on"+type] = callback;
+    }
+};
+
+addEvent(window, "resize", function () {
+    var width = window.innerWidth;
+    this.profilename.style.fontSize = width / 12 + "px";
+    this.widthcounter.textContent = width;
+});
